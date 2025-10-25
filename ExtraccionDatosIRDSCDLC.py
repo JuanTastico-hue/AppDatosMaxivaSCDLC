80b9db645daebfa5ed0d9b0b549c1594d24094db
JuanTastico <juan.vaz.gom@gmail.com>
2025-10-25 09:54:30 -0600

    Prueba de acceso a todas las direccioens IP de los IRD mediante una lista

--- ExtraccionDatosIRDSCDLC.py
+++ ExtraccionDatosIRDSCDLC.py
@@ -6,3 +6,33 @@
 #   \ \________\ \_______\ \__\ \__\ \__\\ \__\   \ \__\ \ \__\ \__\____\_\  \   \ \__\ \ \__\ \_______\ \_______\
 #    \|________|\|_______|\|__|\|__|\|__| \|__|    \|__|  \|__|\|__|\_________\   \|__|  \|__|\|_______|\|_______|
 #                                                                  \|_________|
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+#from webdriver_manager.chrome import ChromeDriverManager
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as ec
+import time
+
+lista_ip_ird = [
+    'http://10.20.39.211/advanced',             #IRD Main Red 2 SCDLC
+    'http://10.20.39.212/advanced',             #IRD Main Red 5 SCDLC
+    'http://10.20.39.213/advanced',             #IRD Main Red 9 SCDLC
+    'http://10.20.39.216/advanced',             #IRD Main Red 4 SCDLC
+    'http://10.20.39.214/advanced',             #IRD BKP Red 2 SCDLC
+    'http://10.20.39.215/advanced',             #IRD BKP Red 5 SCDLC
+    'http://10.20.39.217/advanced',             #IRD BKP Red 4 SCDLC
+]
+
+#service = Service(ChromeDriverManager().install())
+service = Service(r'D:/Proyectos/Rep Tuxtla/Drivers/chromedriver.exe')
+
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+for i in lista_ip_ird:
+    driver.get(i)
+    time.sleep(4)
+
+driver.quit()
